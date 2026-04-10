77791e7c5dcdf48951d463e188aa72f60492d332
Jpedlar <jpedlar@saairlink.co.za>
2026-04-10 14:08:59 +0200

    keep to one

--- git project/Program.cs
+++ git project/Program.cs
@@ -1,4 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Jordan how are you keep modifications going");
-Console.WriteLine("Hello, Jordan how are you keep modifications going");
-Console.WriteLine("Hello, Jordan how are you keep modifications going");
+
