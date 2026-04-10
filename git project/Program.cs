72ba73ad74b449512b3880bd0e923999c1da6550
Jordan Pedlar <jpedlar@saairlink.co.za>
2026-04-10 14:17:09 +0200

    Revert "keep to one"
    This reverts commit 77791e7c5dcdf48951d463e188aa72f60492d332.

--- git project/Program.cs
+++ git project/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Jordan how are you keep modifications going");
-
+Console.WriteLine("Hello, Jordan how are you keep modifications going");
+Console.WriteLine("Hello, Jordan how are you keep modifications going");
